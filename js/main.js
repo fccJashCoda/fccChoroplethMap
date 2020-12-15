74dc3ad373e8480d65ade967b8baae06be016ba9
fccJashCoda <personalrevolution2014@gmail.com>
2020-12-15 19:24:16 +0100

    added a legend

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // todos in no speciific order
-// [ ] display the tooltip next to the mouse (since we don't have x y, use mouseposition ?)
+// [x] display the tooltip next to the mouse
 // [ ] fix display when zooming
-// [ ] redesign the tooltip
+// [x] redesign the tooltip
 // [ ] finish building up html scafold
-// [ ] build the legend
-// [ ] pick another color scheme
+// [x] build the legend
+// [x] pick another color scheme
 // [ ] clean up code
 // [ ] ponder on a better way to access educationData from d3
 
@@ -13,7 +13,6 @@
     // Constants
     const WIDTH = 975;
     const HEIGHT = 610;
-    const PADDING = 120;
 
     // DOM queries
 
@@ -37,32 +36,19 @@
 
     function renderData(countyData, educationData) {
       // Private Functions
-
       const _tooltipHTML = (d) => {
         return `
-          ${d.area_name}, ${d.state}
-            <br>
-            Bachelor or higher: ${d.bachelorsOrHigher}%
+          ${d.area_name}, ${d.state}: ${d.bachelorsOrHigher}%
           `;
       };
 
       //  Data
-      console.log(educationData);
-      // countyData.data.objects.counties.geometries = countyData.data.objects.counties.geometries.map(
-      //   (county, i) => {
-      //     return Object.assign(county, educationData.data[i]);
-      //   }
-      // );
-
+      console.log(d3);
       const us = countyData.data;
-      console.log(us);
-      console.log(topojson);
 
       const path = d3.geoPath();
 
-      const color = d3.scaleQuantize([1, 70], d3.schemeBlues[9]);
-
-      // Axis bars
+      const color = d3.scaleQuantize([1, 70], d3.schemeOranges[9]);
 
       //Tooltip
       const tooltip = d3
@@ -82,23 +68,35 @@
 
       svg
         .append('g')
+        .attr('transform', 'translate(610, 20)')
+        .append(() =>
+          legend({
+            width: 260,
+            color,
+            title: 'bachelors degree or higher (%)',
+            tickFormat: '.0f',
+          })
+        );
+
+      svg
+        .append('g')
         .selectAll('path')
         .data(topojson.feature(us, us.objects.counties).features)
         .join('path')
         .attr('class', 'county')
         .attr('d', path)
         .attr('fill', (d) => {
-          const [edx] = educationData.data.filter(
+          const [education] = educationData.data.filter(
             (county) => county.fips === d.id
           );
-          return color(edx.bachelorsOrHigher);
+          return color(education.bachelorsOrHigher);
         })
         .attr('data-fips', (d, i) => d.id)
         .attr('data-education', (d, i) => {
-          const [edx] = educationData.data.filter(
+          const [education] = educationData.data.filter(
             (county) => county.fips === d.id
           );
-          return edx.bachelorsOrHigher;
+          return education.bachelorsOrHigher;
         });
 
       // Tooltip animation
@@ -114,17 +112,17 @@
         .selectAll('.county')
         .on('mouseover', function (d) {
           d3.select(this).order().raise().style('stroke', 'black');
-          const [edx] = educationData.data.filter(
+          const [education] = educationData.data.filter(
             (county) => county.fips === d.id
           );
 
           const { x, y } = this.getBBox();
-          console.log(x);
-          console.log(this.getBBox());
-          tooltip
 
-            .html(`${_tooltipHTML(edx)}`)
-            .attr('data-year', `${d.year}`)
+          tooltip
+            .html(
+              `${education.area_name}, ${education.state}: ${education.bachelorsOrHigher}%`
+            )
+            .attr('data-education', `${education.bachelorsOrHigher}`)
             .style('visibility', 'visible')
             .style('top', `${y}px`)
             .style('left', `${x + 20}px`);
@@ -135,4 +133,187 @@
         });
     }
   });
+
+  function legend({
+    color,
+    title,
+    tickSize = 6,
+    width = 320,
+    height = 44 + tickSize,
+    marginTop = 18,
+    marginRight = 0,
+    marginBottom = 16 + tickSize,
+    marginLeft = 0,
+    ticks = width / 64,
+    tickFormat,
+    tickValues,
+  } = {}) {
+    const svg = d3
+      .create('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .attr('viewBox', [0, 0, width, height])
+      .style('overflow', 'visible')
+      .style('display', 'block');
+
+    let tickAdjust = (g) =>
+      g.selectAll('.tick line').attr('y1', marginTop + marginBottom - height);
+    let x;
+
+    // Continuous
+    if (color.interpolate) {
+      const n = Math.min(color.domain().length, color.range().length);
+
+      x = color
+        .copy()
+        .rangeRound(
+          d3.quantize(d3.interpolate(marginLeft, width - marginRight), n)
+        );
+
+      svg
+        .append('image')
+        .attr('x', marginLeft)
+        .attr('y', marginTop)
+        .attr('width', width - marginLeft - marginRight)
+        .attr('height', height - marginTop - marginBottom)
+        .attr('preserveAspectRatio', 'none')
+        .attr(
+          'xlink:href',
+          ramp(
+            color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))
+          ).toDataURL()
+        );
+    }
+
+    // Sequential
+    else if (color.interpolator) {
+      x = Object.assign(
+        color
+          .copy()
+          .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+        {
+          range() {
+            return [marginLeft, width - marginRight];
+          },
+        }
+      );
+
+      svg
+        .append('image')
+        .attr('x', marginLeft)
+        .attr('y', marginTop)
+        .attr('width', width - marginLeft - marginRight)
+        .attr('height', height - marginTop - marginBottom)
+        .attr('preserveAspectRatio', 'none')
+        .attr('xlink:href', ramp(color.interpolator()).toDataURL());
+
+      // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+      if (!x.ticks) {
+        if (tickValues === undefined) {
+          const n = Math.round(ticks + 1);
+          tickValues = d3
+            .range(n)
+            .map((i) => d3.quantile(color.domain(), i / (n - 1)));
+        }
+        if (typeof tickFormat !== 'function') {
+          tickFormat = d3.format(tickFormat === undefined ? ',f' : tickFormat);
+        }
+      }
+    }
+
+    // Threshold
+    else if (color.invertExtent) {
+      const thresholds = color.thresholds
+        ? color.thresholds() // scaleQuantize
+        : color.quantiles
+        ? color.quantiles() // scaleQuantile
+        : color.domain(); // scaleThreshold
+
+      const thresholdFormat =
+        tickFormat === undefined
+          ? (d) => d
+          : typeof tickFormat === 'string'
+          ? d3.format(tickFormat)
+          : tickFormat;
+
+      x = d3
+        .scaleLinear()
+        .domain([-1, color.range().length - 1])
+        .rangeRound([marginLeft, width - marginRight]);
+
+      svg
+        .append('g')
+        .selectAll('rect')
+        .data(color.range())
+        .join('rect')
+        .attr('x', (d, i) => x(i - 1))
+        .attr('y', marginTop)
+        .attr('width', (d, i) => x(i) - x(i - 1))
+        .attr('height', height - marginTop - marginBottom)
+        .attr('fill', (d) => d);
+
+      tickValues = d3.range(thresholds.length);
+      tickFormat = (i) => thresholdFormat(thresholds[i], i);
+    }
+
+    // Ordinal
+    else {
+      x = d3
+        .scaleBand()
+        .domain(color.domain())
+        .rangeRound([marginLeft, width - marginRight]);
+
+      svg
+        .append('g')
+        .selectAll('rect')
+        .data(color.domain())
+        .join('rect')
+        .attr('x', x)
+        .attr('y', marginTop)
+        .attr('width', Math.max(0, x.bandwidth() - 1))
+        .attr('height', height - marginTop - marginBottom)
+        .attr('fill', color);
+
+      tickAdjust = () => {};
+    }
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0,${height - marginBottom})`)
+      .call(
+        d3
+          .axisBottom(x)
+          .ticks(ticks, typeof tickFormat === 'string' ? tickFormat : undefined)
+          .tickFormat(typeof tickFormat === 'function' ? tickFormat : undefined)
+          .tickSize(tickSize)
+          .tickValues(tickValues)
+      )
+      .call(tickAdjust)
+      .call((g) => g.select('.domain').remove())
+      .call((g) =>
+        g
+          .append('text')
+          .attr('x', marginLeft)
+          .attr('y', marginTop + marginBottom - height - 6)
+          .attr('fill', 'currentColor')
+          .attr('text-anchor', 'start')
+          .attr('font-weight', 'bold')
+          .attr('class', 'title')
+          .text(title)
+      );
+
+    return svg.node();
+  }
+
+  function ramp(color, n = 256) {
+    var canvas = document.createElement('canvas');
+    canvas.width = n;
+    canvas.height = 1;
+    const context = canvas.getContext('2d');
+    for (let i = 0; i < n; ++i) {
+      context.fillStyle = color(i / (n - 1));
+      context.fillRect(i, 0, 1, 1);
+    }
+    return canvas;
+  }
 })();
